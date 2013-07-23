f45d0e31959d1232c2ebee6e3b890085052c3752
Mohammad Tayseer <m.tayseer82@gmail.com>
2013-07-23 05:15:15 +0200

    Adding static files routes for production server

--- HowManyPeopleFitHere/urls.py
+++ HowManyPeopleFitHere/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -15,3 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
